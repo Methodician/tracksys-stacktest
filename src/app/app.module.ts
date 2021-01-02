e0cee32db63691888ea2a1f47c420064c946a77c
Methodician <methodician@gmail.com>
2021-01-01 16:24:07 -0800

    registration works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
+// ANGULAR GUTS
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+// ANGULAR MATERIAL
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
+// ANGULAR FIRE
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { environment } from 'src/environments/environment';
+
+// INTERNAL COMPONENTS
+import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
+
+// INTERNAL MODULES
+import { AppRoutingModule } from './app-routing.module';
+
+// OTHER INTERNALS
+import { environment } from 'src/environments/environment';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -23,6 +38,11 @@ import { RegisterComponent } from './components/auth/register/register.component
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
     AngularFireAuthModule,
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  authInfo$ = new BehaviorSubject<any>(null);
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) { 
+    this.authInfo$.subscribe(console.log)
+
+    this.afAuth.user.subscribe(user => {
+      if(user) {
+        this.authInfo$.next(user);
+      }else { this.authInfo$.next(null)}
+    })
+  }
+
+  register = (email: string, password: string) => {
+    this.afAuth.createUserWithEmailAndPassword(email, password);
+  }
 }
