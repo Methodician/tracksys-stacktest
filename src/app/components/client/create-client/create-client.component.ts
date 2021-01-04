a23255b6d390a7e12f639ff4b64b3c4fbb8a9dd9
Methodician <methodician@gmail.com>
2021-01-04 12:18:46 -0800

    Validated states with handy, user-friendly auto complete

--- src/app/components/client/create-client/create-client.component.ts
+++ src/app/components/client/create-client/create-client.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import {ClientI} from 'src/app/models/client.models';
-import {ClientService} from 'src/app/services/client.service'
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+import { ClientI, StateCodeM } from 'src/app/models/client.models';
+import { ClientService } from 'src/app/services/client.service';
 
 @Component({
   selector: 'tks-create-client',
   templateUrl: './create-client.component.html',
-  styleUrls: ['./create-client.component.scss']
+  styleUrls: ['./create-client.component.scss'],
 })
 export class CreateClientComponent implements OnInit {
   clientForm: FormGroup;
+  stateCodes: string[];
+  filteredOptions: Observable<string[]>;
 
   constructor(fb: FormBuilder, private clientSvc: ClientService) {
     this.clientForm = fb.group({
@@ -21,25 +25,42 @@ export class CreateClientComponent implements OnInit {
       city: ['', Validators.required],
       state: ['', Validators.required],
       zipCode: ['', Validators.required],
-    })
+    });
+
+    this.stateCodes = Array.from(StateCodeM).map(state => state[0]);
+
+    this.filteredOptions = this.clientForm.controls['state'].valueChanges.pipe(
+      startWith(''),
+      map(val => this._stateFilter(val)),
+    );
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   submitClient = () => {
-    const isActive = this.clientForm.get('isActive')?.value;
-    const name = this.clientForm.get('name')?.value;
-    const formalName = this.clientForm.get('formalName')?.value;
-    const address1 = this.clientForm.get('address1')?.value;
-    const address2 = this.clientForm.get('address2')?.value;
-    const city = this.clientForm.get('city')?.value;
-    const state = this.clientForm.get('state')?.value;
-    const zipCode = this.clientForm.get('zipCode')?.value;
+    if (!this.clientForm.valid) {
+      alert('You made a boo boo');
+    }
+    const client = this.clientForm.value as ClientI;
 
-    const client: ClientI = { isActive, name, formalName, address1, address2, city, state, zipCode};
     this.clientSvc.addClient(client);
+  };
 
-  }
+  checkState = () => {
+    // To really secure data against user error we can also add
+    // custom validators to invalidate the form if a bad state is input
+    // Else, if the user submits 1 second after putting an incorrect state it may go through to DB
+    // Alternatively we can just rely on back-end validation beyond this entry, which I prefer for simplicity and reliability
+    setTimeout(() => {
+      const selection = this.clientForm.get('state')?.value;
+      const isValid = this.stateCodes.includes(selection);
+      if (!isValid) this.clientForm.patchValue({ state: '' });
+    }, 1000);
+  };
 
+  private _stateFilter = (value: string): string[] => {
+    return this.stateCodes.filter(option =>
+      option.toUpperCase().includes(value),
+    );
+  };
 }
