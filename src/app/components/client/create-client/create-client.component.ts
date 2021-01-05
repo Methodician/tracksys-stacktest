d3870276e27f3498203bceacb73a04371299c7d1
Methodician <methodician@gmail.com>
2021-01-05 15:37:16 -0800

    Replaced state code validation with a simple check to match with internal list and added convenience method to get array of state codes from the StateCodeM

--- src/app/components/client/create-client/create-client.component.ts
+++ src/app/components/client/create-client/create-client.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 import { Observable } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
-import { ClientI, StateCodeM } from 'src/app/models/client.models';
+import { ClientI, stateCodesArray } from 'src/app/models/client.models';
 import { ClientService } from 'src/app/services/client.service';
 
 const zipCodeRegex = /(^\d{5}$)|(^\d{5}-\d{4}$)/;
-const stateCodeRegex = /^((A[LKSZR])|(C[AOT])|(D[EC])|(F[ML])|(G[AU])|(HI)|(I[DLNA])|(K[SY])|(LA)|(M[EHDAINSOT])|(N[EVHJMYCD])|(MP)|(O[HKR])|(P[WAR])|(RI)|(S[CD])|(T[NX])|(UT)|(V[TIA])|(W[AVIY]))$/;
 const cityRegex = /^([a-zA-Z\u0080-\u024F]+(?:. |-| |'))*[a-zA-Z\u0080-\u024F]*$/;
 
 @Component({
@@ -16,7 +21,6 @@ const cityRegex = /^([a-zA-Z\u0080-\u024F]+(?:. |-| |'))*[a-zA-Z\u0080-\u024F]*$
 })
 export class CreateClientComponent implements OnInit {
   clientForm: FormGroup;
-  stateCodes: string[];
   filteredOptions: Observable<string[]>;
 
   constructor(fb: FormBuilder, private clientSvc: ClientService) {
@@ -27,11 +31,9 @@ export class CreateClientComponent implements OnInit {
       address1: ['', Validators.required],
       address2: '',
       city: ['', [Validators.required, Validators.pattern(cityRegex)]],
-      state: ['', [Validators.required, Validators.pattern(stateCodeRegex)]],
+      state: ['', [Validators.required, this.matchInternalStateCodeList]],
       zipCode: ['', [Validators.required, Validators.pattern(zipCodeRegex)]],
     });
-
-    this.stateCodes = Array.from(StateCodeM).map(state => state[0]);
 
     this.filteredOptions = this.clientForm.controls['state'].valueChanges.pipe(
       startWith(''),
@@ -54,20 +56,13 @@ export class CreateClientComponent implements OnInit {
     alert(`we would route you to the detail for client with id: ${clientId}`);
   };
 
-  checkState = () => {
-    // To really secure data against user error we can also add
-    // custom validators to invalidate the form if a bad state is input
-    // Else, if the user submits 1 second after putting an incorrect state it may go through to DB
-    // Alternatively we can just rely on back-end validation beyond this entry, which I prefer for simplicity and reliability
-    setTimeout(() => {
-      const selection = this.clientForm.get('state')?.value;
-      const isValid = this.stateCodes.includes(selection);
-      if (!isValid) this.clientForm.patchValue({ state: '' });
-    }, 1000);
-  };
+  matchInternalStateCodeList: ValidatorFn = (control: AbstractControl) =>
+    stateCodesArray.includes(control.value)
+      ? null
+      : { invalidStateCode: control.value };
 
   private _stateFilter = (value: string): string[] => {
-    return this.stateCodes.filter(option =>
+    return stateCodesArray.filter(option =>
       option.toUpperCase().includes(value),
     );
   };
